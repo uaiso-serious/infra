34601ebacd391a03e1cf7423c0997b734aa16c32
Nilzao <288571+nilzao@users.noreply.github.com>
2025-12-22 15:48:25 -0400

    n8n auto setup

--- _setup/basic.sh
+++ _setup/basic.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mkdir -p /mnt/k3s-data /mnt/data/{n8n,ollama} /root/.ssh
+mkdir -p /mnt/k3s-data /mnt/data/{n8n,ollama} /root/.ssh /root/.kube
 chmod -R 777 /mnt/data
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--data-dir /mnt/k3s-data" sh -
+
+cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
 chmod 700 get_helm.sh
@@ -42,7 +44,14 @@ sed -i "s/127\.0\.0\.1/$HOST_IP/g" ./infra/_setup/k3s/ssh-socks5.yaml
 kubectl apply -f ./infra/_setup/k3s/ssh-socks5.yaml
 
 kubectl apply -f ./infra/postgresql/pgvector.yaml
-kubectl rollout status statefulset/postgres -n postgresql
+kubectl -n postgresql rollout status statefulset/postgres
+
 kubectl apply -f ./infra/uaiso.yaml
 
-cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
+kubectl -n uaiso rollout status statefulset/n8n
+kubectl -n uaiso exec n8n-0 -- ash -c "git clone https://github.com/uaiso-serious/infra.git /tmp/infra"
+kubectl -n uaiso exec n8n-0 -- ash -c "n8n import:entities --truncateTables --inputDir=/tmp/infra/n8n/entities --keyFile=/tmp/infra/n8n/keyfile.txt"
+kubectl -n uaiso exec n8n-0 -- ash -c "cd .n8n/nodes;npm i n8n-nodes-xmpp@1.0.121"
+kubectl -n uaiso exec n8n-0 -- ash -c "n8n import:credentials --separate --input=/tmp/infra/n8n/credentials"
+kubectl -n uaiso exec n8n-0 -- ash -c "n8n import:workflow --separate --input=/tmp/infra/n8n/workflows"
+kubectl -n uaiso delete pod n8n-0
--- n8n/README.md
+++ n8n/README.md
@@ -3,17 +3,6 @@ n8n:
 - url: http://n8n.uaiso.lan/
 - volume mount /mnt/data/n8n
 
-# Import initial setup and restart n8n pod:
-
-```bash
-kubectl -n uaiso exec n8n-0 -- ash -c "git clone https://github.com/uaiso-serious/infra.git /tmp/infra"
-kubectl -n uaiso exec n8n-0 -- ash -c "n8n import:entities --truncateTables --inputDir=/tmp/infra/n8n/entities --keyFile=/tmp/infra/n8n/keyfile.txt"
-kubectl -n uaiso exec n8n-0 -- ash -c "cd .n8n/nodes;npm i n8n-nodes-xmpp@1.0.121"
-kubectl -n uaiso exec n8n-0 -- ash -c "n8n import:credentials --separate --input=/tmp/infra/n8n/credentials"
-kubectl -n uaiso exec n8n-0 -- ash -c "n8n import:workflow --separate --input=/tmp/infra/n8n/workflows"
-kubectl -n uaiso delete pod n8n-0
-```
-
 ---
 
 http://n8n.uaiso.lan/
