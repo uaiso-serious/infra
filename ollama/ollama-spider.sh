a150720b0bcceab62e8f799777aa6d5c02f7b8f3
Nilzao <288571+nilzao@users.noreply.github.com>
2025-12-22 17:08:22 -0400

    ollama spider and models.csv

--- ollama/ollama-spider.sh
+++ ollama/ollama-spider.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-#mkdir -p models
-#
-#curl "https://ollama.com/search?o=newest" > models/001_search.html
-#grep "href=\"/library/" models/001_search.html | grep -oP 'href="\K[^"]+' > models/002_search_links.html
-#
-#while read -r link; do
-#  curl "https://ollama.com${link}/tags" > models/$(basename "${link}")-tags.html
-#done < models/002_search_links.html
-#
-#echo "model,name,size,context" > models/tmp.csv
-#
-#for file in models/*-tags.html; do
-#  xmllint --html --xpath "//div[contains(@class,'group')]/div[@class='hidden md:flex flex-col space-y-[6px]']/div[@class='grid grid-cols-12 items-center']" "$file" 2>/dev/null \
-#  | xmllint --html --xpath "//a/text() | //p[@class='col-span-2 text-neutral-500 text-[13px]']/text()" - 2>/dev/null \
-#  | awk 'ORS=NR%3?",":"\n"' \
-#  | awk -v fname="$(basename "$file" -tags.html)" '{print fname","$1","$2","$3}' >> models/tmp.csv
-#done
+mkdir -p models
+
+curl "https://ollama.com/search?o=newest" > models/001_search.html
+grep "href=\"/library/" models/001_search.html | grep -oP 'href="\K[^"]+' > models/002_search_links.html
+
+while read -r link; do
+  curl "https://ollama.com${link}/tags" > models/$(basename "${link}")-tags.html
+done < models/002_search_links.html
+
+echo "model,name,size,context" > models/tmp.csv
+
+for file in models/*-tags.html; do
+  xmllint --html --xpath "//div[contains(@class,'group')]/div[@class='hidden md:flex flex-col space-y-[6px]']/div[@class='grid grid-cols-12 items-center']" "$file" 2>/dev/null \
+  | xmllint --html --xpath "//a/text() | //p[@class='col-span-2 text-neutral-500 text-[13px]']/text()" - 2>/dev/null \
+  | awk 'ORS=NR%3?",":"\n"' \
+  | awk -v fname="$(basename "$file" -tags.html)" '{print fname","$1","$2","$3}' >> models/tmp.csv
+done
 
 awk -F, '{
   size=$3;
