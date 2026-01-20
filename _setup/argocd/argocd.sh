417a45ac6ee2e213693ba14a277101fe4a9d0bf6
Nilzao <288571+nilzao@users.noreply.github.com>
2026-01-20 17:49:34 -0400

    argocd.sh relative paths

--- _setup/argocd/argocd.sh
+++ _setup/argocd/argocd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 kubectl create namespace argocd
 kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/refs/tags/v3.2.5/manifests/install.yaml
-kubectl -n argocd apply -f argocd-custom.yaml
+kubectl -n argocd apply -f infra/_setup/argocd/argocd-custom.yaml
 kubectl -n argocd rollout status deployment/argocd-server
 kubectl -n argocd scale deployment/argocd-server --replicas 0
 kubectl -n argocd rollout status deployment/argocd-server
 kubectl -n argocd scale deployment/argocd-server --replicas 1
 kubectl -n argocd rollout status deployment/argocd-server
-kubectl -n argocd apply -f argocd-basic.yaml
+kubectl -n argocd apply -f infra/_setup/argocd/argocd-basic.yaml
 while true; do
   kubectl -n onedev exec onedev-0 -- bash -c "curl -s -u admin:admin http://onedev.uaiso.lan/~api/version/server" | grep 14.0.7 && break
   sleep 5
@@ -16,4 +16,4 @@ kubectl -n onedev exec onedev-0 -- bash -c "git clone https://github.com/uaiso-s
 
 kubectl -n onedev exec onedev-0 -- bash -c "curl -s -u admin:admin -d@/tmp/infra/_setup/argocd/onedev-new-project.json -H 'Content-Type: application/json' http://onedev.uaiso.lan/~api/projects"
 kubectl -n onedev exec onedev-0 -- bash -c "cd /tmp/infra;git remote rm origin;git remote add origin http://admin:admin@onedev.uaiso.lan/uaiso.git;git push -u origin main"
-kubectl -n argocd apply -f argocd-uaiso.yaml
+kubectl -n argocd apply -f infra/_setup/argocd/argocd-uaiso.yaml
